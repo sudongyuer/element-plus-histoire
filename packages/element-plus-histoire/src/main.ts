f31e4d4133fda7208007f1e9a0230d1509441f3b
Frozen FIsh <976499226@qq.com>
2022-10-13 21:38:24 +0800

    chore: update main.ts

--- packages/element-plus-histoire/src/main.ts
+++ packages/element-plus-histoire/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')

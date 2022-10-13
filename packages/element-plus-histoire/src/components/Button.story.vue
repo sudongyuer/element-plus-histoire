674c960317c6cec7a6c241f090f7c5a971344b86
Frozen FIsh <976499226@qq.com>
2022-10-14 00:00:16 +0800

    chore: update

---
+++ packages/element-plus-histoire/src/components/Button.story.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <Story title="🐱 Meow">
+    🐱
+  </Story>
+</template>
--- packages/element-plus-histoire/vite.config.ts
+++ packages/element-plus-histoire/vite.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="histoire" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  histoire: {
+    // your Histoire configuration
+  },
   plugins: [vue()],
 })
